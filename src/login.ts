ec9c63ce45ba854174ebf9abc69f608c0b8cfffc
Shivam Gupta <shigupt@microsoft.com>
2020-08-04 19:55:13 +0530

    Adding az login dep

--- src/login.ts
+++ src/login.ts
@@ -2,79 +2,41 @@ import * as core from '@actions/core';
 import { issueCommand } from '@actions/core/lib/command';
 import * as path from 'path';
 import * as fs from 'fs';
-import { WebRequest, WebRequestOptions, WebResponse, sendRequest } from "./client";
+import * as exec from "@actions/exec";
+import * as io from "@actions/io";
+import { WebRequest, WebRequestOptions, WebResponse, sendRequest } from './client';
 import * as querystring from 'querystring';
 import * as nonInteractiveLogin from './non-interactive-login';
+import { ExecOptions } from '@actions/exec/lib/interfaces';
+import { ToolRunner } from '@actions/exec/lib/toolrunner';
 
-let isConnectedCluster;
-const SUCCESS = 200;
+const managementEndpointUrl = 'https://management.azure.com/';
 
-function getAzureAccessToken(servicePrincipalId, servicePrincipalKey, tenantId, authorityUrl, managementEndpointUrl: string): Promise<string> {
+async function getAzureAccessToken(): Promise<any> {
+    const azPath = await io.which('az', true);
+    let output = '';
+    let error = '';
+    const options: any = {};
+    options.silent = true;
+    options.listeners = {
+        stdout: (data: Buffer) => {
+            output += data.toString();
+        },
+        stderr: (data: Buffer) => {
+            error += data.toString();
+        },
+    };
 
-    if (!servicePrincipalId || !servicePrincipalKey || !tenantId || !authorityUrl) {
-        throw new Error("Not all values are present in the creds object. Ensure appId, password and tenant are supplied");
+    try {
+        const azToolRunner = new ToolRunner(azPath, ['account', 'get-access-token', `--resource=${managementEndpointUrl}`], options);
+        const azStatus = await azToolRunner.exec();
+        return JSON.parse(output);
+    } catch (error) {
+        throw new Error('Error: Could not fetch azure access token: ' + error);
     }
-    return new Promise<string>((resolve, reject) => {
-        let webRequest = new WebRequest();
-        webRequest.method = "POST";
-        webRequest.uri = `${authorityUrl}/${tenantId}/oauth2/token/`;
-        webRequest.body = querystring.stringify({
-            resource: managementEndpointUrl,
-            client_id: servicePrincipalId,
-            grant_type: "client_credentials",
-            client_secret: servicePrincipalKey
-        });
-        webRequest.headers = {
-            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
-        };
-
-        let webRequestOptions: WebRequestOptions = {
-            retriableStatusCodes: [400, 408, 409, 500, 502, 503, 504],
-        };
-
-        sendRequest(webRequest, webRequestOptions).then(
-            (response: WebResponse) => {
-                if (response.statusCode == 200) {
-                    resolve(response.body.access_token);
-                }
-                else if ([400, 401, 403].indexOf(response.statusCode) != -1) {
-                    reject('ExpiredServicePrincipal');
-                }
-                else {
-                    reject('CouldNotFetchAccessTokenforAzureStatusCode');
-                }
-            },
-            (error) => {
-                reject(error)
-            }
-        );
-    });
 }
 
-function getAKSKubeconfig(azureSessionToken: string, subscriptionId: string, managementEndpointUrl: string): Promise<string> {
-    let resourceGroupName = core.getInput('resource-group', { required: true });
-    let clusterName = core.getInput('cluster-name', { required: true });
-    return new Promise<string>((resolve, reject) => {
-        var webRequest = new WebRequest();
-        webRequest.method = 'GET';
-        webRequest.uri = `${managementEndpointUrl}/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.ContainerService/managedClusters/${clusterName}/accessProfiles/clusterAdmin?api-version=2017-08-31`;
-        webRequest.headers = {
-            'Authorization': 'Bearer ' + azureSessionToken,
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-        sendRequest(webRequest).then((response: WebResponse) => {
-            let accessProfile = response.body;
-            if (accessProfile.properties && accessProfile.properties.kubeConfig) {
-                var kubeconfig = Buffer.from(accessProfile.properties.kubeConfig, 'base64');
-                resolve(kubeconfig.toString());
-            } else {
-                reject(JSON.stringify(response.body));
-            }
-        }).catch(reject);
-    });
-}
-
-function getAKSKubeconfigForARC(azureSessionToken: string, subscriptionId: string, managementEndpointUrl: string): Promise<string> {
+function getARCKubeconfig(azureSessionToken: string, subscriptionId: string, managementEndpointUrl: string): Promise<string> {
     let resourceGroupName = core.getInput('resource-group', { required: true });
     let clusterName = core.getInput('cluster-name', { required: true });
     return new Promise<string>((resolve, reject) => {
@@ -99,52 +61,12 @@ function getAKSKubeconfigForARC(azureSessionToken: string, subscriptionId: strin
     });
 }
 
-function checkClusterType(azureSessionToken: string, subscriptionId: string, managementEndpointUrl: string): Promise<boolean> {
-    let resourceGroupName = core.getInput('resource-group', { required: true });
-    let clusterName = core.getInput('cluster-name', { required: true });
-    return new Promise<boolean>((resolve, reject) => {
-        var webRequest = new WebRequest();
-        webRequest.method = 'GET';
-        webRequest.uri = `${managementEndpointUrl}/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Kubernetes/connectedClusters/${clusterName}?api-version=2020-01-01-preview`;
-        webRequest.headers = {
-            'Authorization': 'Bearer ' + azureSessionToken,
-            'Content-Type': 'application/json; charset=utf-8'
-        }
-        sendRequest(webRequest).then((response: WebResponse) => {
-            let statusCode = response.statusCode;
-            if (statusCode == SUCCESS) {
-                resolve(true);
-            } else {
-                resolve(false);
-            }
-        }).catch(reject);
-    });
-}
-
 async function getKubeconfig(): Promise<string> {
-    let creds = core.getInput('creds', { required: true });
-    let credsObject: { [key: string]: string; };
-    try {
-        credsObject = JSON.parse(creds);
-    } catch (ex) {
-        throw new Error('Credentials object is not a valid JSON');
-    }
+    let azOutput = await getAzureAccessToken();
+    let azureSessionToken = azOutput.accessToken;
+    let subscriptionId = azOutput.subscription;
 
-    let servicePrincipalId = credsObject["clientId"];
-    let servicePrincipalKey = credsObject["clientSecret"];
-    let tenantId = credsObject["tenantId"];
-    let authorityUrl = credsObject["activeDirectoryEndpointUrl"] || "https://login.microsoftonline.com";
-    let managementEndpointUrl = credsObject["resourceManagerEndpointUrl"] || "https://management.azure.com/";
-    let subscriptionId = credsObject["subscriptionId"];
-    let azureSessionToken = await getAzureAccessToken(servicePrincipalId, servicePrincipalKey, tenantId, authorityUrl, managementEndpointUrl);
-    
-    isConnectedCluster = await checkClusterType(azureSessionToken, subscriptionId, managementEndpointUrl);
-    let kubeconfig;
-    if (isConnectedCluster)
-        kubeconfig = await getAKSKubeconfigForARC(azureSessionToken, subscriptionId, managementEndpointUrl);
-    else
-        kubeconfig = await getAKSKubeconfig(azureSessionToken, subscriptionId, managementEndpointUrl);
-
+    let kubeconfig = await getARCKubeconfig(azureSessionToken, subscriptionId, managementEndpointUrl);
     return kubeconfig;
 }
 
@@ -157,10 +79,8 @@ async function run() {
     fs.writeFileSync(kubeconfigPath, kubeconfig);
     issueCommand('set-env', { name: 'KUBECONFIG' }, kubeconfigPath);
     console.log('KUBECONFIG environment variable is set');
-    if (isConnectedCluster) {
-        await nonInteractiveLogin.login(kubeconfigPath);
-        console.log('Kubeconfig is updated with AAD access token');
-    }
+    await nonInteractiveLogin.login(kubeconfigPath);
+    console.log('Kubeconfig is updated with AAD access token');
 }
 
 run().catch(core.setFailed);
